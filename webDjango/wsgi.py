f1d2a83f64ef0fb1c0c632ba009157415845dd8a
Derick J. Carcamo <derick_vlk_@hotmail.es>
2022-09-28 00:00:58 -0600

    clase24

--- webDjango/wsgi.py
+++ webDjango/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webDjango.settings')
 
 application = get_wsgi_application()
+
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webDjango.settings")
+application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
