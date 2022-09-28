cc3450c79fc4f6848e16c917c84c485376a95e5f
Derick J. Carcamo <derick_vlk_@hotmail.es>
2022-09-28 01:39:53 -0600

    Clase 24.4

--- webDjango/wsgi.py
+++ webDjango/wsgi.py
@@ -15,9 +15,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webDjango.settings')
 
 application = get_wsgi_application()
 
-import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webDjango.settings")
-application = get_wsgi_application()
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+#import os
+#from django.core.wsgi import get_wsgi_application
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webDjango.settings")
+#application = get_wsgi_application()
+#from whitenoise.django import DjangoWhiteNoise
+#application = DjangoWhiteNoise(application)
